7213f2da0adf5da584874f002d264e4af776e5b1
SantiagoRR2004 <114857078+SantiagoRR2004@users.noreply.github.com>
2025-12-02 19:41:15 +0100

    Add options

--- main.sh
+++ main.sh
@@ -16,8 +16,8 @@ cp "$VIDEO" DeepFaceLab/workspace/data_dst.mp4
 # Copy the picture to data_src folder
 cp "$PICTURE" DeepFaceLab/workspace/data_src/00001.png
 
-# Extract images from the video
-cd DeepFaceLab/scripts && source ./3_extract_image_from_data_dst.sh && cd ..
+# Extract images from the video (png format)
+cd DeepFaceLab/scripts && echo "png" | source ./3_extract_image_from_data_dst.sh && cd ../..
 
-# Extract faces from the source images
-cd DeepFaceLab/scripts && source ./4_data_src_extract_faces_S3FD.sh && cd ..
+# Extract faces from the source images (whole face) (1 face per image) (512 image size) (90 jepeg quality) (no debug)
+cd DeepFaceLab/scripts && printf "wf\n1\n512\n90\nn\n" | source ./4_data_src_extract_faces_S3FD.sh && cd ..
