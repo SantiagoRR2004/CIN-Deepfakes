43c9d2a3d05ba9b2575a8ceab8726ffd383acd9b
SantiagoRR2004 <114857078+SantiagoRR2004@users.noreply.github.com>
2025-12-06 20:49:16 +0100

    Add more steps

--- main.sh
+++ main.sh
@@ -24,3 +24,23 @@ cd DeepFaceLab/scripts && printf "wf\n1\n512\n90\nn\n" | source ./4_data_src_ext
 
 # Extract faces from the destination images (whole face) (1 face per image) (512 image size) (90 jepeg quality) (no debug)
 cd DeepFaceLab/scripts && printf "wf\n1\n512\n90\nn\n" | source ./5_data_dst_extract_faces_S3FD.sh && cd ../..
+
+# Train the model (Quick96) - automatically stops after the timeout
+cd DeepFaceLab/scripts && source env.sh && \
+printf "face\n" | \
+timeout --signal=SIGINT --kill-after=30 180 $DFL_PYTHON "$DFL_SRC/main.py" train \
+    --training-data-src-dir "$DFL_WORKSPACE/data_src/aligned" \
+    --training-data-dst-dir "$DFL_WORKSPACE/data_dst/aligned" \
+    --pretraining-data-dir "$DFL_SRC/pretrain_CelebA" \
+    --pretrained-model-dir "$DFL_SRC/pretrain_Quick96" \
+    --model-dir "$DFL_WORKSPACE/model" \
+    --model Quick96 \
+    --no-preview \
+    --silent-start && \
+cd ../..
+
+# Merge the faces into the destination images (the name of the model is "face")
+cd DeepFaceLab/scripts && source ./7_merge_Quick96.sh && cd ../..
+
+Convert the merged images back to video
+cd DeepFaceLab/scripts && source ./8_merged_to_mp4.sh && cd ../..
