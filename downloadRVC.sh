f467d73c00cd812c3f4332b1123995f98e7f7bf0
SantiagoRR2004 <114857078+SantiagoRR2004@users.noreply.github.com>
2025-12-08 14:56:48 +0100

    Fix download

--- downloadRVC.sh
+++ downloadRVC.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Set the repository URL and directory
-REPO_URL="clone https://github.com/RVC-Project/Retrieval-based-Voice-Conversion"
+REPO_URL="https://github.com/RVC-Project/Retrieval-based-Voice-Conversion"
 DIR_NAME="RVC"
 
 ./downloadRepository.sh "$REPO_URL" "$DIR_NAME"
